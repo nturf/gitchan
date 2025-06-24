3c460ca41f0019171604429162ecbf6fadd4392e
nturf <thestraybyte@gmail.com>
2025-06-25 05:28:27 +0530

    setup shadcn

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button"
+export default async function Home() {
+    return (
+        <div>
+            <Button>Click</Button>
+            <h1 className="text-red-500">Hey</h1>
+        </div>
+    )
+}
